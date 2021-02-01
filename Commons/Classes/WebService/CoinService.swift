fb1727534da16dd9fa27bdda22c6e1506e42e268
Artur <Artur>
2021-02-01 10:40:03 -0300

    teste

--- Commons/Classes/WebService/CoinService.swift
+++ Commons/Classes/WebService/CoinService.swift
@@ -9,7 +9,7 @@ import UIKit
 import Alamofire
 
 public protocol CoinsServiceProtocol {
-    func getCoins(completion:@escaping (_ coins: Welcome, _ error: String?) -> Void )
+    public func getCoins(completion:@escaping (_ coins: Welcome, _ error: String?) -> Void )
 }
 
 public class CoinService: CoinsServiceProtocol {
